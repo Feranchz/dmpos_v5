b043765d209f114b7d522fcb9845c9e688054d80
José Hernández <joseghi.2608@gmail.com>
2019-10-22 13:21:13 -0400

    Focus en el campo usuario al iniciar el log in, detalles en vista y orden

--- javascript/login.js
+++ javascript/login.js
@@ -1,13 +1,14 @@
 $(document).ready(function(){
+	$("#username").focus();
 	$("#FormLogin").submit((e)=>{
 		e.preventDefault();
 		var user = $("#username").val();
 		var pass = $("#password").val();
 
-		$("#username").val("");
+		$("#username").val("").focus();
 		$("#password").val("");
 
-		console.log("Log in");
+		console.log("Log in.");
 	})
 })
 
