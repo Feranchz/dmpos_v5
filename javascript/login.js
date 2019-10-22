fd2b77e8010b4361558199fcfe0e354167348d98
José Hernández <joseghi.2608@gmail.com>
2019-10-22 00:15:12 -0400

    Redireccionamiento al hacer login .2

--- javascript/login.js
+++ javascript/login.js
@@ -3,9 +3,12 @@ $(document).ready(function(){
 })
 
 function login(){
+	window.location='http://159.203.83.182/DMPOS/develop/admin/adm.php';
+	alert("Redireccionando")
 	var usuario= $("#username").val();
 	var password=$("#password").val();
 	console.log(usuario+" "+password);
-	document.location.href="http://159.203.83.182/DMPOS/develop/admin/adm.php";
-
+	
+	console.log("redireccionando");
+	
 }
