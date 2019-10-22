667891558f812d98a9f73de2799e712e698b2dec
José Hernández <joseghi.2608@gmail.com>
2019-10-22 18:41:37 -0400

    Pagina despues de login

--- javascript/login.js
+++ javascript/login.js
@@ -7,9 +7,41 @@ $(document).ready(function(){
 
 		$("#username").val("").focus();
 		$("#password").val("");
-
+		login(user);
 		console.log("Log in.");
 	})
 })
+
+function login(datos){
+	$.ajax({
+		url:'admin/sesion.html',
+		success:function(sesion){
+			$("body").html(sesion)
+			sesionIniciada();
+		}
+	});
+	return false;
+}
+function sesionIniciada(){
+	setInterval(()=>{
+		var fecha=new Date(),
+			horas=fecha.getHours(),
+			minutos=fecha.getMinutes(),
+			diaSemana=fecha.getDay(),
+			dia=fecha.getDate(),
+			mes=fecha.getMonth(),
+			year=fecha.getFullYear();
+		var semana=['Dom','Lun','Mar','Mie','Jue','Vie','Sab']
+		var meses=['Ene','Feb','Mar','Abr','May','Jun','Jul','Ago','Seb','Oct','Nov','Dic']
+		var impf=semana[diaSemana]+" "+dia+" "+meses[mes]+" "+year+" "+horas+":"+minutos;
+		$("#fecha").html(impf);
+	},1000);
+		$('.dropdown-trigger').dropdown({
+			constrainWidth:true,
+			coverTrigger:false
+		});
+		 $('.collapsible').collapsible();
+
+}
 
 
