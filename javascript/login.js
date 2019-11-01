8c6ad939022fff9c2f3755ca61731909304d1490
José Hernández <joseghi.2608@gmail.com>
2019-11-01 09:41:04 -0400

    #22 boton cerrar sesion, #28 boton nuevo producto: abrir modal inferior izquierdo

--- javascript/login.js
+++ javascript/login.js
@@ -9,11 +9,38 @@ $(document).ready(function(){
 		if(true){
 			doLogin()
 		}
-
 	})
+
+	$('#salirSinCorte').click((e)=>{
+		doLogout(e.target.id);
+	})
+	$('#salirConCorte').click((e)=>{
+		doLogout(e.target.id);
+	})
+
+
 })
 
 function doLogin(){
 	$("#main-logeado").show()
 	$("#main-login").hide()
+}
+
+function doLogout(tipo){
+	if(tipo=="salirSinCorte"){
+		console.log("se presiono salir sin corte")	
+		location.reload()
+	}else if(tipo=="salirConCorte"){
+		console.log("se presiono salir con corte")
+
+		$('#modalRealizarCorte').modal({
+			onOpenEnd:function(){
+				$('#500').focus()
+			}
+		})
+		$('#haceCorte').click(e=>{
+			location.reload()
+		})
+	}
+
 }
